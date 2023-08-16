9d480fd5e6384d4fd48b3026f6168252e7cda0f8
Ildar Burkhanov <burhanoff1992@gmail.com>
2023-08-16 20:18:26 +0300

    second commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    const hero_slider = new Swiper(".hero-slider", {
+        slidesPerView: 1,
+        loop: true,
+        autoplayDisableOnInteraction: false,
+        effect: "fade",
+
+        autoplay: {
+            delay: 3000
+        },
+
+        navigation: {
+            nextEl: ".hero-next"
+        },
+
+        pagination: {
+            el: ".hero-pagination",
+            type: "bullets",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return `
+                    <div class="hero-pagination-slide ${className}">
+                        <div class="hero-pagination-title">${$(".hero-slide")[index].dataset.slideTitle}</div>
+                        <div class="hero-pagination-progress"><span></span></div>
+                    </div>
+                `;
+            }
+        },
+    });
+});
