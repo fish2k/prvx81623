323a472875e5683a0a778b0332c223855ebcb7bd
Ildar Burkhanov <burhanoff1992@gmail.com>
2023-08-18 20:44:12 +0300

    9 blocks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // $(".header, .hero, .company, .services").hide();
+    // $(".header, .hero, .company, .services, .consult, .specs, .quote, .cases, .partners").hide();
 
     const hero_slider = new Swiper(".hero-slider", {
         slidesPerView: 1,
@@ -29,4 +29,46 @@ $(document).ready(function() {
             }
         },
     });
+
+    const specs_slider = new Swiper(".specs-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: ".spec-next",
+            prevEl: ".spec-prev",
+        }
+    });
+
+    const reviews_slider = new Swiper(".reviews-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: ".reviews-next",
+            prevEl: ".reviews-prev",
+        }
+    });
+
+    const cases_slider = new Swiper(".cases-slider", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+
+        navigation: {
+            nextEl: ".case-next",
+            prevEl: ".case-prev",
+        }
+    });
+
+    const partners_slider = new Swiper(".partners-slider", {
+        slidesPerView: 6,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: ".partners-next",
+            prevEl: ".partners-prev",
+        }
+    });
+
+    Fancybox.bind("[data-fancybox]", {});
 });
